ba6eeed5359b5f44b16026fc05c8839f8153e23b
semihorhan <sorhan3.14@gmail.com>
2021-11-28 17:42:06 +0300

    fix err for spelling mistakes

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ from difflib import get_close_matches
 data = json.load(open("data.json"))
 
 def translate(w):
-    w = w.lower()
-    if w in data:
-        return data[w]
-    elif w.title() in data:
-        return data[w.title()]
-    elif len(get_close_matches(w, data.keys())) > 0:
+    w_forms = [w.lower(), w.title(), w.upper()] 
+    for form in w_forms:
+        if form in data:
+            return data[form]
+            
+    if len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
         if yn == 'Y':
             return data[get_close_matches(w, data.keys())[0]]
